d2db85779ebb0fd3695fc4f70aeaecd91ced8dff
Michael Spiegel <michael@hushmesh.com>
2024-07-29 20:05:34 -0400

    convert contextual library to no_std

---
+++ contextual/src/lib.rs
@@ -0,0 +1,80 @@
+//! A small crate to deal with data in context.
+//!
+//! ```
+//! use contextual::{WithContext, AsRefWithContext, DisplayWithContext};
+//! use std::fmt;
+//!
+//! /// Index of an element in some array.
+//! pub struct Index(usize);
+//!
+//! impl<T, C: AsRef<[T]>> AsRefWithContext<T, C> for Index {
+//!   fn as_ref_with<'a>(&'a self, context: &'a C) -> &'a T {
+//!     &context.as_ref()[self.0]
+//!   }
+//! }
+//!
+//! impl<'a, C: AsRef<[&'a str]>> DisplayWithContext<C> for Index {
+//!   fn fmt_with(&self, context: &C, f: &mut fmt::Formatter) -> fmt::Result {
+//!     use fmt::Display;
+//!     context.as_ref()[self.0].fmt(f)
+//!   }
+//! }
+//!
+//! let i = Index(1);
+//! let context = ["a", "b", "c"];
+//!
+//! print!("index: {}", i.with(&context));
+//! assert_eq!(*i.with(&context).as_ref(), "b")
+//! ```
+#![cfg_attr(not(feature = "std"), no_std)]
+
+extern crate alloc;
+
+mod as_ref;
+mod display;
+mod from;
+mod into_ref;
+mod try_from;
+
+pub use as_ref::*;
+pub use display::*;
+pub use from::*;
+pub use into_ref::*;
+pub use try_from::*;
+
+pub struct Contextual<T, C>(pub T, pub C);
+
+pub trait WithContext {
+	fn with<C>(&self, context: C) -> Contextual<&Self, C>;
+
+	fn into_with<C>(self, context: C) -> Contextual<Self, C>
+	where
+		Self: Sized;
+}
+
+impl<T: ?Sized> WithContext for T {
+	fn with<C>(&self, context: C) -> Contextual<&Self, C> {
+		Contextual(self, context)
+	}
+
+	fn into_with<C>(self, context: C) -> Contextual<Self, C>
+	where
+		Self: Sized,
+	{
+		Contextual(self, context)
+	}
+}
+
+impl<T, C> core::ops::Deref for Contextual<T, C> {
+	type Target = T;
+
+	fn deref(&self) -> &T {
+		&self.0
+	}
+}
+
+impl<T, C> core::ops::DerefMut for Contextual<T, C> {
+	fn deref_mut(&mut self) -> &mut T {
+		&mut self.0
+	}
+}
---
+++ contextual/src/try_from.rs
@@ -0,0 +1,29 @@
+use crate::IntoWithContext;
+
+pub trait TryFromWithContext<U, C>: Sized {
+	type Error;
+
+	fn try_from_with(value: U, context: &C) -> Result<Self, Self::Error>;
+}
+
+impl<T, U: IntoWithContext<T, C>, C> TryFromWithContext<U, C> for T {
+	type Error = core::convert::Infallible;
+
+	fn try_from_with(value: U, context: &C) -> Result<Self, Self::Error> {
+		Ok(value.into_with(context))
+	}
+}
+
+pub trait TryIntoWithContext<U, C> {
+	type Error;
+
+	fn try_into_with(self, context: &C) -> Result<U, Self::Error>;
+}
+
+impl<T, U: TryFromWithContext<T, C>, C> TryIntoWithContext<U, C> for T {
+	type Error = U::Error;
+
+	fn try_into_with(self, context: &C) -> Result<U, Self::Error> {
+		U::try_from_with(self, context)
+	}
+}
