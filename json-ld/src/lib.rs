2217e1165fa16a17c6c68f0042a82134e0504176
Michael Spiegel <michael@hushmesh.com>
2024-08-19 08:50:14 -0400

    compilation fixes

--- json-ld/src/lib.rs
+++ json-ld/src/lib.rs
@@ -309,11 +309,13 @@ extern crate alloc;
 
 extern crate thiserror_nostd_notrait as thiserror;
 
+pub use json_ld_compaction as compaction;
 pub use json_ld_context_processing as context_processing;
 pub use json_ld_core::*;
 pub use json_ld_expansion as expansion;
 pub use json_ld_syntax as syntax;
 
+pub use compaction::Compact;
 pub use context_processing::Process;
 pub use expansion::Expand;
 
